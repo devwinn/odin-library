77a654b3eaa2c1091ae12d7a594aa39bb0a50983
devwinn <80548344+devwinn@users.noreply.github.com>
2025-01-26 16:06:35 -0700

    functions working from console but not running upon submit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 
 const library = [];
 const bookCreator = document.getElementById('book-creator');
+const bookSubmit = document.getElementById('form-submit')
 const libraryCards = document.getElementById('library-cards');
+
+
 
 function addToLibrary(book) {
     library.push(book);
@@ -19,18 +22,36 @@ function createBook(author, pages, read, title) {
 }
 
 function displayBooks(library) {
+
     library.forEach(element => {
         let cardHtml = `
-        <div class="book-card">
             <h2>${element.title}</h2>
             <h3>${element.author}</h3>
-            <p>${pages} pages</p>
-            <p>${read}</p>
-        </div>
-    `;
-    libraryCards.append(cardHtml);
+            <p>${element.pages} pages</p>
+            <p>${element.read}</p>
+        `;
+        let insertCard = document.createElement("div");
+
+        insertCard.classList.add("book-card");
+        insertCard.innerHTML = cardHtml;
+        libraryCards.append(insertCard);
+        console.log(element.info);
     }
 );
 
 
+}
+
+function submitBook () {
+    let newAuthor = document.getElementById('author');
+    let newPages = document.getElementById('pages');
+    let newRead = document.forms.create.elements.read['value'];
+    let newTitle = document.getElementById('title');
+
+    if (newAuthor && newPages && newRead && newTitle) {
+        let newBook = new createBook(newAuthor, newPages, newRead, newTitle);
+        addToLibrary(newBook);
+    }
+
+    console.log(library)
 }
